6c02f84bcc89ad1dfecf5853c3e8ae8c205e92a8
kamranjamali <kamranjamali365@gmail.com>
2023-09-07 17:11:38 +0500

    added new

--- JavaClass.java
+++ JavaClass.java
@@ -1,9 +1,14 @@
 import java.util.*;
 public class JavaClass{
 	public static void main(String sd[]){
-int a=1;	
+
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();	
 System.out.println("I am HERE");
 System.out.println("I AM CHANED");
 System.out.println(new Date());
+System.out.println("Addition : "+(a+b));
+
 }
 }
